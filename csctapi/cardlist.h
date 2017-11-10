b94cd2a916e5b4a3499647daa226196cc807994b
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2017-11-10 20:36:49 +0100

    Tryfix

--- csctapi/cardlist.h
+++ csctapi/cardlist.h
@@ -32,7 +32,7 @@ unity_02 =	{ "Unitymedia 02", 0, 12, "3F FF 95 00 FF 91 81 71 FE 47 00 44 4E 41 
 hdplus01 =	{ "HD-Plus 01", 0, 12, "3F FF 95 00 FF 91 81 71 FE 47 00 44 4E 41 53 50 31 34 32 20 52 65 76 47 43 36 61",    80, 0, "\xA7\x64\x2F\x57\xBC\x96\xD3\x7C\x00", "\xBF\x35\x8B\x54\x61\x86\x31\x30\x68\x6F\xC9\x33\xFB\x54\x1F\xFC\xED\x68\x2F\x36\x80\xF0\x9D\xBC\x1A\x23\x82\x9F\xB3\xB2\xF7\x66\xB9\xDD\x1B\xF3\xB3\xEC\xC9\xAD\x66\x61\xB7\x53\xDC\xC3\xA9\x62\x41\x56\xF9\xEB\x64\xE8\x16\x8E\xF0\x9E\x4D\x9C\x5C\xCA\x4D\xD5\x00", "\x00", "" },
 hdplus02 =	{ "HD-Plus 02", 0, 12, "3F FF 95 00 FF 91 81 71 A0 47 00 44 4E 41 53 50 31 38 30 20 4D 65 72 30 30 30 28",    80, 0, "\xA7\x64\x2F\x57\xBC\x96\xD3\x7C\x00", "\xBF\x35\x8B\x54\x61\x86\x31\x30\x68\x6F\xC9\x33\xFB\x54\x1F\xFC\xED\x68\x2F\x36\x80\xF0\x9D\xBC\x1A\x23\x82\x9F\xB3\xB2\xF7\x66\xB9\xDD\x1B\xF3\xB3\xEC\xC9\xAD\x66\x61\xB7\x53\xDC\xC3\xA9\x62\x41\x56\xF9\xEB\x64\xE8\x16\x8E\xF0\x9E\x4D\x9C\x5C\xCA\x4D\xD5\x00", "\x00", "" },
 hdplus03 =	{ "HD-Plus 03", 0, 12, "3F FF 95 00 FF 91 81 71 A0 47 00 44 4E 41 53 50 31 39 30 20 4D 65 72 51 32 35 4F",    80, 0, "\xA7\x64\x2F\x57\xBC\x96\xD3\x7C\x00", "\x90\x1E\x59\x51\x52\xE6\x7D\xFD\x5B\x13\x4E\x1D\x19\x5C\x41\x41\xB3\xBB\x13\x94\xA8\xAF\x4D\x6B\xF1\xD1\x08\x5D\xCC\x4D\x9C\xBA\x5C\x73\xA0\x6E\xD2\x1F\xC3\x55\x6B\x68\x54\x98\x03\x0B\xB1\x18\x57\x66\x11\x75\x65\xE3\x99\x95\xEF\xBF\x72\x13\x5C\x28\x17\xB7\x00", "\x00", "" },
-redlight =	{ "Redlight Elite", 0, 0, "3F 77 18 00 00 C2 EB 41 02 6C 90 00",                                                   30, 0, "\x01\x4D\x90\x2F\x00", "\x00", "\x22\xD6\x0F\x37\x56\x7B\xAB\x12\xF5\x05\xD8\xC8\xC7\x1A\x22\xE3\x00", "" },
+redlight =	{ "Redlight Elite", 0, 0, "3F 77 18 00 00 C2 EB 41 02 6C 90 00",                                              35, 0, "\x01\x4D\x90\x2F\x00", "\x00", "\x22\xD6\x0F\x37\x56\x7B\xAB\x12\xF5\x05\xD8\xC8\xC7\x1A\x22\xE3\x00", "" },
 tntviav5 =	{ "TNT Viaccess V5", 0, 0, "3F 77 18 00 00 C2 EB 41 02 6C",                                                   30, 0, "\x01\x4D\x90\x2F\x00", "\x00", "\x22\xD6\x0F\x37\x56\x7B\xAB\x12\xF5\x05\xD8\xC8\xC7\x1A\x22\xE3\x00", "0500@030B00:439726EBB6A939A456C05FF6AA606C43,F1DCB15A3DE3FA1D7E2998DA7DD4898A,48C19B86A4E2EB7288DFDCE7C2BB7577,0,0,0,0,0,0,0,0,0,9A3EAB0203EBFFCA85B4F18280749F56,2504B382B16D8C6758DB960E311E9351,349883E54D58336DCA750A878ACC5CD5,4A26AD251795A58A11BBC07B53C44348,2844258792F6D9529A328B3E8CD2FD0E,F62C3BE300E98BBB378DFA38BB6EEEF1,0,0,6B1B024D36F60974973CB81FA5E8F01C,127003F8E95100367A556121C779FB6E,9D73BE61D6498E2F20E157E1C6416A23,1DE5B042AD670BB16A3A76BDAD2F7AC0,6747F2E500CF123428337591435C6585" },
 tntviav6 =	{ "TNT Viaccess V6", 0, 0, "3F 77 18 00 00 D3 8A 40 01 64",                                                   30, 0, "\x01\x4D\x90\x2F\x00", "\x00", "\x22\xD6\x0F\x37\x56\x7B\xAB\x12\xF5\x05\xD8\xC8\xC7\x1A\x22\xE3\x00", "0500@030B00:439726EBB6A939A456C05FF6AA606C43,F1DCB15A3DE3FA1D7E2998DA7DD4898A,48C19B86A4E2EB7288DFDCE7C2BB7577,0,0,0,0,0,0,0,0,0,9A3EAB0203EBFFCA85B4F18280749F56,2504B382B16D8C6758DB960E311E9351,349883E54D58336DCA750A878ACC5CD5,4A26AD251795A58A11BBC07B53C44348,2844258792F6D9529A328B3E8CD2FD0E,F62C3BE300E98BBB378DFA38BB6EEEF1,0,0,6B1B024D36F60974973CB81FA5E8F01C,127003F8E95100367A556121C779FB6E,9D73BE61D6498E2F20E157E1C6416A23,1DE5B042AD670BB16A3A76BDAD2F7AC0,6747F2E500CF123428337591435C6585" };
 
@@ -83,14 +83,14 @@ void findatr(struct s_reader *reader) {
 	} else if ( strncmp(current.atr, redlight.atr, redlight.atrsize) == 0 ) {
 		memcpy(current.providername, redlight.providername, strlen(redlight.providername));
 		memcpy(reader->boxkey, redlight.boxkey, 5);
-		memcpy(reader->des_key, redlight.deskey, 33);
+		memcpy(reader->deskey, redlight.deskey, 33);
 		memcpy(reader->pincode, "0000\0", 5);
-		reader->des_key_length = 16;
+//		reader->des_key_length = 16;
 		current.found = 1;
 	} else if ( strncmp(current.atr, tntviav5.atr, tntviav5.atrsize) == 0 ) {
 		memcpy(current.providername, tntviav5.providername, strlen(tntviav5.providername));
 		memcpy(reader->boxkey, tntviav5.boxkey, 5);
-		memcpy(reader->des_key, tntviav5.deskey, 33);
+		memcpy(reader->deskey, tntviav5.deskey, 33);
 		memcpy(reader->pincode, "0000\0", 5);
 		parse_aes_keys(reader, tntviav5.aeskeys);
 		reader->des_key_length = 16;
@@ -98,7 +98,7 @@ void findatr(struct s_reader *reader) {
 	} else if ( strncmp(current.atr, tntviav6.atr, tntviav6.atrsize) == 0 ) {
 		memcpy(current.providername, tntviav6.providername, strlen(tntviav6.providername));
 		memcpy(reader->boxkey, tntviav6.boxkey, 5);
-		memcpy(reader->des_key, tntviav6.deskey, 33);
+		memcpy(reader->deskey, tntviav6.deskey, 33);
 		memcpy(reader->pincode, "0000\0", 5);
 		parse_aes_keys(reader, tntviav6.aeskeys);
 		reader->des_key_length = 16;
