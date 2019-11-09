0f373492c560674adbdf5f8f0d720ea2956724be
WXbet <root@builder.galaxie.lan>
2019-11-09 16:54:03 +0100

    fix error not using keys from config if no built in keys present correct memcpy byte count statement on some readers

--- csctapi/cardlist.c
+++ csctapi/cardlist.c
@@ -116,74 +116,83 @@ void findatr(struct s_reader *reader) {
 	} else if ( strncmp(current.atr, "3B 9F 21 0E 49 52 44 45 54 4F 20 41 43 53 03 84 55 FF 80 6D", 59) == 0 ) {
 #ifdef READER_NAGRA
 		strcpy(current.providername,"Vodafone D0x Ix2\x0");
-		if ( !strncmp(vf_rsakey, "0", 1) == 0 ) {
-			memcpy(reader->boxkey, vf_boxkey, 8);
-			memcpy(reader->rsa_mod, vf_rsakey, 64);
-			reader->boxkey_length = 8;
-			reader->rsa_mod_length = 64;
+		if ( strlen(vf_rsakey) > 0 ) {
+			memcpy(reader->boxkey,	vf_boxkey,	  9);
+			memcpy(reader->rsa_mod, vf_rsakey,	 65);
+			
+			reader->boxkey_length	=   8;
+			reader->rsa_mod_length	=  64;
+		} else {
+			rdr_log(reader, "no keys built in, use config values boxkey + rsakey");
 		}
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 12 | reader->blockemm);
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= (12 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system NAGRA not built in!\x0");
 #endif
 	} else if ( strncmp(current.atr, "3F 77 18 00 00 C2 EB 41 02 6C 90 00", 35) == 0 ) {
 #ifdef READER_VIACCESS
 		strcpy(current.providername,"Redlight Mega Elite\x0");
-		if ( !strncmp(rlme_deskey, "0", 1) == 0 ) {
-			memcpy(reader->boxkey, rlme_boxkey, 4);
-			memcpy(reader->des_key, rlme_deskey, 16);
-			reader->boxkey_length = 4;
-			reader->des_key_length = 16;
+		if ( strlen(rlme_deskey) > 0 ) {
+			memcpy(reader->boxkey,	rlme_boxkey,  5);
+			memcpy(reader->des_key, rlme_deskey, 17);
+			
+			reader->boxkey_length	=   4;
+			reader->des_key_length	=  16;
+		} else {
+			rdr_log(reader, "no keys built in, use config values boxkey + deskey");
 		}
 		memcpy(reader->pincode, "0000\0", 5);
-		reader->disablecrccws = 1;
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 12 | reader->blockemm);
+		reader->disablecrccws		= 1;
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= (12 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system VIACCESS not built in!\x0");
 #endif
 	} else if ( strncmp(current.atr, "3F 77 18 00 00 D3 8A 42 01 64 90 00", 35) == 0 ) {
 #ifdef READER_VIACCESS
 		strcpy(current.providername,"Redlight Mega Royale\x0");
-		if ( !strncmp(rlmr_deskey, "0", 1) == 0 ) {
-			memcpy(reader->boxkey, rlmr_boxkey, 4);
-			memcpy(reader->des_key, rlmr_deskey, 16);
-			reader->boxkey_length = 4;
-			reader->des_key_length = 16;
+		if ( strlen(rlmr_deskey) > 0 ) {
+			memcpy(reader->boxkey,	rlmr_boxkey,  5);
+			memcpy(reader->des_key, rlmr_deskey, 17);
+
+			reader->boxkey_length	=   4;
+			reader->des_key_length	=  16;
+		} else {
+			rdr_log(reader, "no keys built in, use config values boxkey + deskey");
 		}
 		memcpy(reader->pincode, "0000\0", 5);
-		reader->disablecrccws = 1;
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 12 | reader->blockemm);
+		reader->disablecrccws		= 1;
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= (12 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system VIACCESS not built in!\x0");
 #endif
 	} else if ( strncmp(current.atr, "3B 24 00 30 42 30 30", 20) == 0 ) {
 #ifdef READER_CONAX
 		strcpy(current.providername,"MTV Unlimited\x0");
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 8 | reader->blockemm);
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= ( 8 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system CONAX not built in!\x0");
 #endif
 	} else if ( strncmp(current.atr, "3B 78 12 00 00 54 C4 03 00 8F F1 90 00", 38) == 0 ) {
 #ifdef READER_CRYPTOWORKS
 		strcpy(current.providername,"Cryptoworks\x0");
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 8 | reader->blockemm);
-		reader->needsglobalfirst = 1;
+		reader->needsglobalfirst 	= 1;
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= ( 8 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system CRYPTOWORKS not built in!\x0");
 #endif
 	} else if ( strncmp(current.atr, "3B F7 11 00 01 40 96 70 70 0A 0E 6C B6 D6", 42) == 0 ) {
 #ifdef READER_SECA
 		strcpy(current.providername,"Canal Digitaal (NL)\x0");
-		reader->caid = 0x0100;
-		reader->ratelimitecm = 4;
-		reader->ratelimittime = 9000;
-		reader->saveemm = ( 3 | reader->saveemm);
-		reader->blockemm = ( 12 | reader->blockemm);
+		reader->caid 				= 0x0100;
+		reader->ratelimitecm 		= 4;
+		reader->ratelimittime 		= 9000;
+		reader->saveemm				= ( 3 | reader->saveemm);
+		reader->blockemm			= (12 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system SECA not built in!\x0");
 #endif
@@ -196,26 +205,26 @@ void findatr(struct s_reader *reader) {
 	} else if ( strncmp(current.atr, "3F 77 18 00 00 C2 7A 41 02 68", 29) == 0 ) {
 #ifdef READER_VIACCESS
 		strcpy(current.providername,"SRG v4\x0");
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 8 | reader->blockemm);
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= ( 8 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system VIACCESS not built in!\x0");
 #endif
 	} else if ( strncmp(current.atr, "3F 77 18 00 00 C2 7A 44 02 68", 29) == 0 ) {
 #ifdef READER_VIACCESS
 		strcpy(current.providername,"SRG v5\x0");
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 8 | reader->blockemm);
-		reader->read_old_classes = 0;
+		reader->read_old_classes	= 0;
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= ( 8 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system VIACCESS not built in!\x0");
 #endif
 	} else if ( strncmp(current.atr, "3F 77 18 00 00 D3 8A 40 01 64", 29) == 0 ) {
 #ifdef READER_VIACCESS
 		strcpy(current.providername,"SRG v6\x0");
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 8 | reader->blockemm);
-		reader->read_old_classes = 0;
+		reader->read_old_classes	= 0;
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= ( 8 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system VIACCESS not built in!\x0");
 #endif
@@ -228,39 +237,44 @@ void findatr(struct s_reader *reader) {
 	} else if ( strncmp(current.atr, "3B 9F 21 0E 49 52 44 45 54 4F 20 41 43 53 20 56 35 2E 33 9E", 59) == 0 ) {
 #ifdef READER_IRDETO
 		strcpy(current.providername,"Ziggo NL\x0");
-		if ( !strncmp(znl_boxkey, "0", 1) == 0 ) {
-			memcpy(reader->boxkey, znl_boxkey, 9);
-			memcpy(reader->rsa_mod, znl_rsakey, 65);
-			reader->boxkey_length = 8;
-			reader->rsa_mod_length = 64;
+		if ( strlen(znl_boxkey) > 0 ) {
+			memcpy(reader->boxkey,	znl_boxkey,	  9);
+			memcpy(reader->rsa_mod,	znl_rsakey,  65);
+			
+			reader->boxkey_length	=   8;
+			reader->rsa_mod_length	=  64;
+		} else {
+			rdr_log(reader, "no keys built in, use config values boxkey + rsakey");
 		}
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 12 | reader->blockemm);
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= (12 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system IRDETO not built in!\x0");
 #endif
 	} else if ( strncmp(current.atr, "3F FF 95 00 FF 91 81 71 FE 57 00 44 4E 41 53 50 34 38 32 20 52 65 76 52 32 36 1C", 80) == 0 ) {
 #ifdef READER_NAGRA_MERLIN
 		strcpy(current.providername,"Max TV\x0");
-		if ( !strncmp(max_cwpk, "0", 1) == 0 ) {
-			memcpy(reader->data50, data50,  80);
-			memcpy(reader->mod50,  mod50,  80);
-			memcpy(reader->key60,  key60,  96);
-			memcpy(reader->exp60,  exp60,  96);
-			memcpy(reader->mod1,   mod1, 112);
-			memcpy(reader->nuid,   max_nuid,   4);
-			memcpy(reader->cwekey0, max_cwpk,  16);
+		if ( strlen(max_cwpk) > 0 ) {
+			memcpy(reader->data50,	data50,		 81);
+			memcpy(reader->mod50,	mod50,  	 81);
+			memcpy(reader->key60,	key60,  	 97);
+			memcpy(reader->exp60,	exp60,  	 97);
+			memcpy(reader->mod1,	mod1, 		113);
+			memcpy(reader->nuid,	max_nuid,	  5);
+			memcpy(reader->cwekey0,	max_cwpk,	 17);
 
-			reader->data50_length =  80;
-			reader->mod50_length  =  80;
-			reader->key60_length  =  96;
-			reader->exp60_length  =  96;
-			reader->mod1_length   = 112;
-			reader->nuid_length   =   4;
-			reader->cwekey0_length =  16;
+			reader->data50_length	=  80;
+			reader->mod50_length	=  80;
+			reader->key60_length	=  96;
+			reader->exp60_length	=  96;
+			reader->mod1_length		= 112;
+			reader->nuid_length		=   4;
+			reader->cwekey0_length	=  16;
+		} else {
+			rdr_log(reader, "no keys built in, use config values data50 + mod50 + key60 + exp60 + mod1 + nuid + cwekey0");
 		}
-		reader->saveemm = ( 0 | reader->saveemm);
-		reader->blockemm = ( 8 | reader->blockemm);
+		reader->saveemm				= ( 0 | reader->saveemm);
+		reader->blockemm			= ( 8 | reader->blockemm);
 #else
 		strcpy(current.info, "- but card system NAGRA_MERLIN not built in!\x0");
 #endif
@@ -272,79 +286,96 @@ void findatr(struct s_reader *reader) {
 		if ( ishdold == 1 ) {
 #ifdef READER_NAGRA
 			// Astra HD01 / HD02
-			if ( !strncmp(hd_rsakey, "0", 1) == 0 ) {
-				memcpy(reader->boxkey, hd_boxkey, 9);
-				memcpy(reader->rsa_mod, hd_rsakey, 65);
-				reader->boxkey_length = 8;
-				reader->rsa_mod_length = 64;
+			if ( strlen(hd_rsakey) > 0 ) {
+				memcpy(reader->boxkey, 	hd_boxkey, 	  9);
+				memcpy(reader->rsa_mod, hd_rsakey, 	 65);
+				
+				reader->boxkey_length	=   8;
+				reader->rsa_mod_length	=  64;
+			} else {
+				rdr_log(reader, "no keys built in, use config values boxkey + rsakey");
 			}
-			reader->saveemm = ( 0 | reader->saveemm);
-			reader->blockemm = ( 12 | reader->blockemm);
-			reader->cak7_mode = 0;
+			reader->cak7_mode			= 0;
+			reader->saveemm				= ( 0 | reader->saveemm);
+			reader->blockemm			= (12 | reader->blockemm);
 #else
 			strcpy(current.info, "- but card system NAGRA not built in!\x0");
 #endif
 		} else if ( ishdnew == 1 ) {
 #ifdef READER_NAGRA_MERLIN
 			// Astra HD03 / HD03a / HD03b / HD04 / HD04a / HD04b / HD04h / HD05a
-			if ( !strncmp(hd_cwpk, "0", 1) == 0 ) {
-				memcpy(reader->data50,  data50,      80);
-				memcpy(reader->mod50,   mod50,       80);
-				memcpy(reader->mod1,    mod1,       112);
-				memcpy(reader->nuid,    hd_nuid,      4);
-				memcpy(reader->cwekey0, hd_cwpk,     16);
-				memcpy(reader->key3588, hd_key3588, 136);
-				memcpy(reader->idird,   hd_idird,     4);
-
-				reader->data50_length =   80;
-				reader->mod50_length  =   80;
-				reader->mod1_length   =  112;
-				reader->nuid_length   =    4;
-				reader->cwekey0_length =  16;
-				reader->key3588_length = 136;
-				reader->idird_length =     4;
+			if ( strlen(hd_cwpk) > 0 ) {
+				memcpy(reader->data50,  data50,      81);
+				memcpy(reader->mod50,   mod50,       81);
+				memcpy(reader->mod1,    mod1,       113);
+				memcpy(reader->nuid,    hd_nuid,      5);
+				memcpy(reader->cwekey0, hd_cwpk,     17);
+				memcpy(reader->key3588, hd_key3588, 137);
+				memcpy(reader->idird,   hd_idird,     5);
+				
+				reader->data50_length	=  80;
+				reader->mod50_length	=  80;
+				reader->mod1_length		= 112;
+				reader->nuid_length		=   4;
+				reader->cwekey0_length	=  16;
+				reader->key3588_length	= 136;
+				reader->idird_length	=   4;
+			} else {
+				rdr_log(reader, "no keys built in, use config values data50 + mod50 + mod1 + nuid + cwekey0 + key3588 + idird");
 			}
-			reader->saveemm = ( 0 | reader->saveemm);
-			reader->blockemm = ( 8 | reader->blockemm);
-			reader->cak7_mode = 1;
+			reader->cak7_mode			= 1;
+			reader->saveemm				= ( 0 | reader->saveemm);
+			reader->blockemm			= ( 8 | reader->blockemm);
 #else
 			strcpy(current.info, "- but card system NAGRA_MERLIN not built in!\x0");
 #endif
 		} else if ( isum == 1 ) {
 #ifdef READER_NAGRA
-			if ( !strncmp(um_rsakey, "0", 1) == 0 ) {
-				memcpy(reader->boxkey, um_boxkey, 9);
-				memcpy(reader->rsa_mod, um_rsakey, 65);
-				reader->boxkey_length = 8;
-				reader->rsa_mod_length = 64;
+			// Unitymedia UM01 / UM02
+			if ( strlen(um_rsakey) > 0 ) {
+				memcpy(reader->boxkey, 	um_boxkey, 	  9);
+				memcpy(reader->rsa_mod, um_rsakey, 	 65);
+				
+				reader->boxkey_length	=   8;
+				reader->rsa_mod_length	=  64;
+			} else {
+				rdr_log(reader, "no keys built in, use config values boxkey + rsakey");
 			}
-			reader->saveemm = ( 0 | reader->saveemm);
-			reader->blockemm = ( 12 | reader->blockemm);
+			reader->saveemm				= ( 0 | reader->saveemm);
+			reader->blockemm			= (12 | reader->blockemm);
 #else
 			strcpy(current.info, "- but card system NAGRA not built in!\x0");
 #endif
 		} else if ( istivu == 1 ) {
 #ifdef READER_NAGRA
-			if ( !strncmp(tivu_rsakey, "0", 1) == 0 ) {
-				memcpy(reader->rsa_mod, tivu_rsakey, 120);
-				reader->rsa_mod_length = 120;
+			// Tivusat 183D / 183E
+			if ( strlen(tivu_rsakey) > 0 ) {
+				memcpy(reader->rsa_mod, tivu_rsakey, 	121);
+				
+				reader->rsa_mod_length	= 120;
+			} else {
+				rdr_log(reader, "no keys built in, use config value rsakey");
 			}
-			reader->saveemm = ( 0 | reader->saveemm);
-			reader->blockemm = ( 8 | reader->blockemm);
+			reader->saveemm				= ( 0 | reader->saveemm);
+			reader->blockemm			= ( 8 | reader->blockemm);
 #else
 			strcpy(current.info, "- but card system NAGRA not built in!\x0");
 #endif
 		} else if ( istnt == 1 ) {
 #ifdef READER_VIACCESS
-			if ( !strncmp(tnt_deskey, "0", 1) == 0 ) {
-				memcpy(reader->boxkey, tnt_boxkey, 4);
-				memcpy(reader->des_key, tnt_deskey, 16);
-				reader->boxkey_length = 4;
-				reader->des_key_length = 16;
+			// TNT Viaccess v5 / v6
+			if ( strlen(tnt_deskey) > 0 ) {
+				memcpy(reader->boxkey, 	tnt_boxkey, 	  5);
+				memcpy(reader->des_key, tnt_deskey, 	 17);
+				
+				reader->boxkey_length	=   4;
+				reader->des_key_length	=  16;
+			} else {
+				rdr_log(reader, "no keys built in, use config values boxkey + deskey");
 			}
 			memcpy(reader->pincode, "0000\0", 5);
-			reader->saveemm = ( 0 | reader->saveemm);
-			reader->blockemm = ( 0 | reader->blockemm);
+			reader->saveemm				= ( 0 | reader->saveemm);
+			reader->blockemm			= ( 0 | reader->blockemm);
 #else
 			strcpy(current.info, "- but card system VIACCESS not built in!\x0");
 #endif
@@ -358,22 +389,22 @@ void findatr(struct s_reader *reader) {
 #ifdef READER_VIDEOGUARD
 			if ( strncmp(current.atr, buf, 65) == 0 ) {
 				strcpy(current.providername,"Sky Deutschland V13\x0");
-				reader->disablecrccws = 1;
-				reader->saveemm = ( 1 | reader->saveemm);
-				reader->blockemm = 15;
+				reader->caid				= 0x09C4;
+				reader->disablecrccws		= 1;
 				if ( !reader->boxid ) {reader->boxid = 0x12345678;}
-				reader->caid = 0x09C4;
+				reader->saveemm				= ( 1 | reader->saveemm);
+				reader->blockemm			= 15;
 				current.found = 1;
 				break;
 			}
 			snprintf(buf, 63, "3F FD %i 25 02 50 80 0F 41 B0 0A 69 FF 4A 50 F0 00 00 50 31 03", i);
 			if ( strncmp(current.atr, buf, 62) == 0 ) {
 				strcpy(current.providername,"Sky Deutschland V14\x0");
-				reader->saveemm = ( 1 | reader->saveemm);
-				reader->blockemm = 15;
+				reader->caid				= 0x098C;
+				reader->disablecrccws		= 1;
 				if ( !reader->boxid ) {reader->boxid = 0x12345678;}
-				reader->disablecrccws = 1;
-				reader->caid = 0x098C;
+				reader->saveemm				= ( 1 | reader->saveemm);
+				reader->blockemm			= 15;
 				current.found = 1;
 				break;
 			}
@@ -381,11 +412,11 @@ void findatr(struct s_reader *reader) {
 			snprintf(buf, 63, "3F FD %i 25 02 50 80 0F 55 B0 02 69 FF 4A 50 F0 80 00 50 31 03", i);
 			if ( strncmp(current.atr, buf, 62) == 0 ) {
 				strcpy(current.providername,"Sky Deutschland V15\x0");
-				reader->saveemm = ( 1 | reader->saveemm);
-				reader->blockemm = 15;
+				reader->caid				= 0x098D;
+				reader->disablecrccws		= 1;
 				if ( !reader->boxid ) {reader->boxid = 0x12345678;}
-				reader->disablecrccws = 1;
-				reader->caid = 0x098D;
+				reader->saveemm				= ( 1 | reader->saveemm);
+				reader->blockemm			= 15;
 				current.found = 1;
 				break;
 			}
@@ -393,18 +424,18 @@ void findatr(struct s_reader *reader) {
 			snprintf(buf, 66, "3F FF %i 25 03 10 80 54 B0 01 69 FF 4A 50 70 00 00 4B 57 01 00 00", i);
 			if ( strncmp(current.atr, buf, 65) == 0 ) {
 				strcpy(current.providername,"Sky/Unitymedia V23\x0");
-				reader->saveemm = ( 0 | reader->saveemm);
-				reader->blockemm = ( 12 | reader->blockemm);
 				if ( !reader->boxid ) {reader->boxid = 0x12345678;}
+				reader->saveemm				= ( 0 | reader->saveemm);
+				reader->blockemm			= (12 | reader->blockemm);
 				current.found = 1;
 				break;
 			}
 			snprintf(buf, 63, "3F FD %i 25 02 50 00 03 33 B0 15 69 FF 4A 50 F0 80 03 4B 4C 03", i);
 			if ( strncmp(current.atr, buf, 62) == 0 ) {
 				strcpy(current.providername,"Vodafone G09\x0");
-				reader->saveemm = ( 0 | reader->saveemm);
-				reader->blockemm = ( 12 | reader->blockemm);
 				if ( !reader->boxid ) {reader->boxid = 0x12345678;}
+				reader->saveemm				= ( 0 | reader->saveemm);
+				reader->blockemm			= (12 | reader->blockemm);
 				reader->deprecated = 1;
 				current.found = 1;
 				break;
