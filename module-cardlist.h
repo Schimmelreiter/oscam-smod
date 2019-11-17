5ac7653fb04fc54d90a8a300c89d451b2a5488b7
Recruit3 <you@example.com>
2019-11-17 16:18:09 +0100

    Cardlist support

--- module-cardlist.h
+++ module-cardlist.h
@@ -1,5 +1,10 @@
 #ifndef _CSCTAPI_CARDLIST_H_
 #define _CSCTAPI_CARDLIST_H_
+
+#ifdef WITH_CARDLIST
+
 struct atrlist{ int found; char providername[32]; char atr[80]; char info[50];};
 void findatr(struct s_reader *reader);
 #endif
+
+#endif // WITH_CARDLIST
